2c7527c68a430678efd9f2bcb245f55e4a0923d2
CollegeWork-ship <wood2ej@mail.uc.edu>
2025-09-19 12:55:05 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+int birthMonth = 21;
+                    if (birthMonth >= 1 && birthMonth <= 12) {
+                    System.out.println("Your birth month is " + birthMonth);
+                                                             }
+                    else {
+                    System.out.println("You entered an incorrect month value: " + birthMonth);
+                         }
+        }
+    }
